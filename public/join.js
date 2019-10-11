bbc75f082e126ee0a979167124546890299ad58e
mcdoash <mcdoash@users.noreply.github.com>
2019-10-11 12:40:57 -0400

    Set up express and socket.io

---
+++ public/join.js
@@ -0,0 +1,16 @@
+let socket = io();
+/*let socket = null;
+
+function joinGame() {
+    let name = document.forms["join-game"]["name"].value;
+    
+    if(name == null) {
+        alert("Please enter a name");
+    }
+    else {
+        if(socket == null) {
+            socket.io();
+            socket.on("newuser", addUser);
+        }
+    }
+}*/
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const http = require("http");
+const express = require("express");
+let app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-let server = http.createServer(function (req, res) {
+app.use(express.static('public'))
+
+io.on("connection", function(socket) {
+    console.log("A connection was made.");
     
-});
+    socket.on("disconnect", () => {
+        console.log("Somebody left.");
+    })
+})
 
 server.listen(3000);
 console.log("Server running on port 3000");
