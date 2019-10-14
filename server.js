83897533f5c9cc4dd25e0867d094332fef15f57a
mcdoash <mcdoash@users.noreply.github.com>
2019-10-14 18:33:21 -0400

    Rooms work now :)

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const request = require("request");
 
-let userNum = 0;
-let questions;
 let rooms = [];
+let currentRoom = [];
 
 
 //serve static files
@@ -19,14 +18,14 @@ io.on("connection", function(socket) {
     
     socket.on("disconnect", () => {
         //disconnection, decrease number of users
-        userNum--;
+        rooms[socket.roomIndex].userNum--;
         
         //REMOVE USER
         
         console.log(socket.username + " left.");
         
         //if there are no users connected
-        if(userNum === 0){
+        if(rooms[socket.roomIndex].userNum === 0){
             console.log("No users connected.")
             //stop game
         }
@@ -57,31 +56,37 @@ io.on("connection", function(socket) {
     
     socket.on("createRoom", name => {
         //room name is the id of the client who created it
-        rooms.push({id: ("room-" + socket.id), userNum: 1, numAnswered: 0});
-        socket.join("room-" + socket.id);
+        rooms.push({id: ("room-" + socket.id), userNum: 0, numAnswered: 0, qNum: 0, questions: []});
         
         socket.username = name;
-        initQuestions(setUpUser);
+        
+        socket.join(("room-" + socket.id), () => { 
+            setUpUser();
+        });
     });
-    
     
     function setUpUser() {
         socket.score = 0;
-        socket.roomId = Object.values(socket.rooms)[1];
+        socket.room = Object.values(socket.rooms)[1];
         
-        console.log(socket.username + " joined " + socket.roomId + "!");
+        console.log(socket.username + " joined " + socket.room + "!");
         
         //find index of room array socket is in to change values
-        let index = rooms.findIndex(r => r.id == socket.roomId);
+        let index = rooms.findIndex(r => r.id == socket.room);
         
-        socket.room = rooms[index];
-        socket.room[userNum] += 1;
+        socket.roomIndex = index;
+        rooms[socket.roomIndex].userNum++;
         
         //display question to client
-        socket.emit("newQuestion", questions[qNum]);
-    
+        if(rooms[socket.roomIndex].userNum == 1) {
+            initQuestions(emitQ);
+        }
+        else {
+            emitQ();
+        }
+        
         //display client to others users
-        socket.broadcast.to(socket.roomId).emit("addScore", {id: socket.id, name: socket.username, score: socket.score});
+        socket.broadcast.to(socket.room).emit("addScore", {id: socket.id, name: socket.username, score: socket.score});
         
         //add client to game
         socket.emit("addUser", {name: socket.username, score: socket.score});
@@ -93,7 +98,7 @@ io.on("connection", function(socket) {
         
         for(let i in sockets) {
             //if the sockets are not the same and they're not in the same room, add score
-            if(sockets[i].id != socket.id && sockets[i].room === socket.roomId) {
+            if(sockets[i].id != socket.id && sockets[i].room == socket.room) {
                 socket.emit("addScore", {id: sockets[i].id, name: sockets[i].username, score: sockets[i].score});
                 //answered styling
             }
@@ -109,55 +114,62 @@ io.on("connection", function(socket) {
             socket.score = 0;
         }
         //show others client has answered
-        socket.broadcast.to(socket.roomId).emit("userAnswered", socket.id);
+        socket.broadcast.to(socket.room).emit("userAnswered", socket.id);
         
         //show others client's new score
-        socket.broadcast.to(socket.roomId).emit("updateScore", {id: socket.id, name: socket.username, score: socket.score});
+        socket.broadcast.to(socket.room).emit("updateScore", {id: socket.id, name: socket.username, score: socket.score});
         
         //update client score on their page
         socket.emit("updateClientScore", socket.score);
         
-        numAnswered++;
+        rooms[socket.roomIndex].numAnswered++;
         
         //if everyone has answered, move on to the next question
-        if(numAnswered === userNum) {
-            qNum++;
-            numAnswered = 0; //reset
+        if(rooms[socket.roomIndex].numAnswered === rooms[socket.roomIndex].userNum) {
+            rooms[socket.roomIndex].qNum++;
+            rooms[socket.roomIndex].numAnswered = 0; //reset
             
             //end of round
-            if(qNum === 5){
+            if(rooms[socket.roomIndex].qNum === 5){
+        
+                //reset values for new round
+                rooms[socket.roomIndex].qNum = 0;
+                rooms[socket.roomIndex].numAnswered = 0;
+                
                 initQuestions(startRound);
             }
             else {
                 //reset answered
-                io.to(socket.roomId).emit("resetAnswered");
+                io.to(socket.room).emit("resetAnswered");
                 
-                io.to(socket.roomId).emit("newQuestion", questions[qNum]);
+                io.to(socket.room).emit("newQuestion", rooms[socket.roomIndex].questions[rooms[socket.roomIndex].qNum]);
             }
         }
     });
+    
+    function initQuestions(callback) {
+        //request questions from OTDB
+        request("https://opentdb.com/api.php?amount=5", {json: true}, (err, res, body) => {
+            if(err) {
+                alert("Request failed.");
+            }
+            //set questions
+            rooms[socket.roomIndex].questions = body.results;
+            callback();
+        });
+    }
+
+    function startRound() {
+        io.to(socket.room).emit("newQuestion", rooms[socket.roomIndex].questions[rooms[socket.roomIndex].qNum]);
+    }
+    
+    function emitQ() {
+        socket.emit("newQuestion", rooms[socket.roomIndex].questions[rooms[socket.roomIndex].qNum]);
+    }
 });
 
 
-function initQuestions(callback) {
-    //reset values for new round
-    qNum = 0;
-    numAnswered = 0;
 
-    //request questions from OTDB
-    request("https://opentdb.com/api.php?amount=5", {json: true}, (err, res, body) => {
-        if(err) {
-            alert("Request failed.");
-        }
-        //set questions
-        questions = body.results;
-        callback();
-    });
-}
-
-function startRound() {
-    io.to(socket.roomId).emit("newQuestion", questions[qNum]);
-}
 
 server.listen(3000);
 console.log("Server running on port 3000");
