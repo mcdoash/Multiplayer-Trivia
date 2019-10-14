58ad86f4e8e3764a3411370a4705cb83e99ae62d
mcdoash <mcdoash@users.noreply.github.com>
2019-10-14 13:04:38 -0400

    Cleanup

--- server.js
+++ server.js
@@ -47,71 +47,53 @@ io.on("connection", function(socket) {
             socket.emit("alert", "Invalid code");
         }
         else {
-            let room = code;
-            socket.join(room, () => {
-                let rooms = Object.keys(socket.rooms);
-                console.log("1: " + rooms); // [ <socket.id>, 'room 237' ]
-                socket.username = name;
+            socket.username = name;
+            
+            socket.join(code, () => {
                 setUpUser();
-              });
+            });
         }
     });
     
     socket.on("createRoom", name => {
         //room name is the id of the client who created it
         rooms.push({id: ("room-" + socket.id), userNum: 1, numAnswered: 0});
-        let room = "room-" + socket.id;
-        socket.join(room, () => {
-                let rooms = Object.keys(socket.rooms);
-                console.log(rooms); // [ <socket.id>, 'room 237' ]
-              });
+        socket.join("room-" + socket.id);
+        
         socket.username = name;
         initQuestions(setUpUser);
     });
     
-    /*set base vaules for new user
-    socket.on("newUser", name => {
-        //start game if this is the first user
-        if(userNum === 1) {
-            initQuestions(setUpUser(name));
-        }
-        else {
-            setUpUser(name);
-        }
-    });*/
     
     function setUpUser() {
         socket.score = 0;
-        socket.room = Object.values(socket.rooms)[1];
-        console.log("2: " + Object.values(socket.rooms)[1]);
+        socket.roomId = Object.values(socket.rooms)[1];
         
-        console.log(socket.username + " joined " + socket.room + "!");
+        console.log(socket.username + " joined " + socket.roomId + "!");
         
-        let index = rooms.findIndex(r => r.id == socket.room);
-        console.log(":) \t" + index + "\n\nARRAY");
+        //find index of room array socket is in to change values
+        let index = rooms.findIndex(r => r.id == socket.roomId);
         
-        for(let i=0; i<rooms.length; i++) {
-            console.log(rooms[i].id);
-        }
+        socket.room = rooms[index];
+        socket.room[userNum] += 1;
         
-        rooms[index][userNum] += 1;
-        console.log("Annnnnd.... " + rooms[index].userNum);
+        //display question to client
+        socket.emit("newQuestion", questions[qNum]);
+    
+        //display client to others users
+        socket.broadcast.to(socket.roomId).emit("addScore", {id: socket.id, name: socket.username, score: socket.score});
         
         //add client to game
         socket.emit("addUser", {name: socket.username, score: socket.score});
 
-        //display client to others users
-        socket.broadcast.to(socket.room).emit("addScore", {id: socket.id, name: socket.username, score: socket.score});
-
-        //display question to client
-        socket.emit("newQuestion", questions[qNum]);
     }
     
     socket.on("initScores", id => {
         let sockets = io.sockets.sockets;
         
         for(let i in sockets) {
-            if(sockets[i].id != socket.id) {
+            //if the sockets are not the same and they're not in the same room, add score
+            if(sockets[i].id != socket.id && sockets[i].room === socket.roomId) {
                 socket.emit("addScore", {id: sockets[i].id, name: sockets[i].username, score: sockets[i].score});
                 //answered styling
             }
@@ -127,10 +109,10 @@ io.on("connection", function(socket) {
             socket.score = 0;
         }
         //show others client has answered
-        socket.broadcast.to(socket.room).emit("userAnswered", socket.id);
+        socket.broadcast.to(socket.roomId).emit("userAnswered", socket.id);
         
         //show others client's new score
-        socket.broadcast.to(socket.room).emit("updateScore", {id: socket.id, name: socket.username, score: socket.score});
+        socket.broadcast.to(socket.roomId).emit("updateScore", {id: socket.id, name: socket.username, score: socket.score});
         
         //update client score on their page
         socket.emit("updateClientScore", socket.score);
@@ -148,9 +130,9 @@ io.on("connection", function(socket) {
             }
             else {
                 //reset answered
-                io.to(socket.room).emit("resetAnswered");
+                io.to(socket.roomId).emit("resetAnswered");
                 
-                io.to(socket.room).emit("newQuestion", questions[qNum]);
+                io.to(socket.roomId).emit("newQuestion", questions[qNum]);
             }
         }
     });
@@ -174,7 +156,7 @@ function initQuestions(callback) {
 }
 
 function startRound() {
-    io.to(socket.room).emit("newQuestion", questions[qNum]);
+    io.to(socket.roomId).emit("newQuestion", questions[qNum]);
 }
 
 server.listen(3000);
